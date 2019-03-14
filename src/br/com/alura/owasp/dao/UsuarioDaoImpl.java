af264ab1960a7f9b182fdb864b8b369022526c10
Jefferson Ribeiro de Medeiros <jeffrm@gmail.com>
2019-03-13 23:47:10 -0300

    Aula 3 - Salvando senha com hash

--- src/br/com/alura/owasp/dao/UsuarioDaoImpl.java
+++ src/br/com/alura/owasp/dao/UsuarioDaoImpl.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.owasp.model.Usuario;
@@ -15,7 +16,17 @@ public class UsuarioDaoImpl implements UsuarioDao {
 	EntityManager manager;
 
 	public void salva(Usuario usuario) {
+		transformaASenhaDoUsuarioEmHash(usuario);
 		manager.persist(usuario);
+	}
+
+	public void transformaASenhaDoUsuarioEmHash(Usuario usuario) {
+
+		String salto = BCrypt.gensalt();// O salto é um valor aleatório gerado o qual será adicionado a entrada padrão
+										// da senha com o objetivo de gerar o código hash
+		String senhaHashed = BCrypt.hashpw(usuario.getSenha(), salto);
+		usuario.setSenha(senhaHashed);
+
 	}
 
 	public Usuario procuraUsuario(Usuario usuario) {
@@ -23,11 +34,25 @@ public class UsuarioDaoImpl implements UsuarioDao {
 		// a execução da query e com isso o banco já sabe como a query está estruturada.
 		// Somente depois os parâmetros são substituídos pelos dados vindos do
 		// formulário como sendo meramente valores.
-		TypedQuery<Usuario> query = manager.createQuery("select u from Usuario u where u.email=:email and u.senha=:senha", Usuario.class);
+		TypedQuery<Usuario> query = manager.createQuery("select u from Usuario u where u.email=:email", Usuario.class);
 		query.setParameter("email", usuario.getEmail());
-		query.setParameter("senha", usuario.getSenha());
 
 		Usuario usuarioRetornado = query.getResultList().stream().findFirst().orElse(null);
-		return usuarioRetornado;
+
+		// passado o usuario do form e o usuario retornado pela consulta do banco
+		if (validaASenhaDoUsuarioComOHashDoBanco(usuario, usuarioRetornado)) {
+			return usuarioRetornado;
+		}
+
+		return null;
+	}
+
+	private boolean validaASenhaDoUsuarioComOHashDoBanco(Usuario usuario, Usuario usuarioRetornado) {
+
+		if (usuarioRetornado == null) {
+			return false;
+		}
+		return BCrypt.checkpw(usuario.getSenha(), usuarioRetornado.getSenha());
+
 	}
 }
