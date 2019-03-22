ebd68a50a00451ccc72f87a6bace072d60c6ed3f
Jefferson Ribeiro de Medeiros <jeffrm@gmail.com>
2019-03-21 22:21:00 -0300

    Parte 2 - Aula 4 - Tratando Mass Assignment com setAllowedFields do Spring

--- src/br/com/alura/owasp/controller/UsuarioController.java
+++ src/br/com/alura/owasp/controller/UsuarioController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +21,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.alura.owasp.dao.UsuarioDao;
 import br.com.alura.owasp.model.Role;
 import br.com.alura.owasp.model.Usuario;
-import br.com.alura.owasp.model.UsuarioDTO;
 import br.com.alura.owasp.retrofit.GoogleWebClient;
 
 @Controller
@@ -31,6 +32,14 @@ public class UsuarioController {
 
 	@Autowired
 	private GoogleWebClient cliente;
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.setAllowedFields("nome", "email", "senha", "nomeImagem");// recurso do Spring nos oferece o tratamento de
+																		// Mass Assingment
+		// especificando que somente esses quatro atributos é que tem permissão de serem
+		// manipulados no form
+	}
 
 	@RequestMapping("/usuario")
 	public String usuario(Model model) {
@@ -45,9 +54,7 @@ public class UsuarioController {
 	}
 
 	@RequestMapping(value = "/registrar", method = RequestMethod.POST)
-	public String registrar(MultipartFile imagem, @ModelAttribute("usuarioRegistro") UsuarioDTO usuarioDTORegistro, RedirectAttributes redirect, HttpServletRequest request, Model model, HttpSession session) throws IllegalStateException, IOException {
-
-		Usuario usuarioRegistro = usuarioDTORegistro.montaUsuario();
+	public String registrar(MultipartFile imagem, @ModelAttribute("usuarioRegistro") Usuario usuarioRegistro, RedirectAttributes redirect, HttpServletRequest request, Model model, HttpSession session) throws IllegalStateException, IOException {
 
 		tratarImagem(imagem, usuarioRegistro, request);
 		usuarioRegistro.getRoles().add(new Role("ROLE_USER"));
